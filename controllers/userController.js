07a3d30d23e71d076495e1c394d0c7358e0da59e
Emox133 <emir.betaacc0unt@gmail.com>
2021-06-03 13:48:04 +0200

    route restrictions

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,3 +14,14 @@ exports.getAllUsers = catchAsync(async(req, res, next) => {
         users
     })
 })
+
+exports.getMe = catchAsync(async(req, res, next) => {
+    const user = await User.find({_id: req.user._id})
+
+    if(!user) return next(new AppError('Korisnik nije pronađen', 404))
+
+    res.status(200).json({
+        message: 'success',
+        user
+    })
+})
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -9,10 +9,12 @@ router.route('/signup')
 router.route('/login')
 .post(authController.login)
 
-// ADMINSTRATOR ROUTES ONLY
+// PROTECTED ROUTES BELLOW
 router.use(authController.protectRoutes)
+router.route('/me').get(userController.getMe)
+
+// ADMINSTRATOR ROUTES ONLY
 router.use(authController.restrictTo('admin'))
-router.route('/')
-.get(userController.getAllUsers)
+router.route('/').get(userController.getAllUsers)
 
 module.exports = router
